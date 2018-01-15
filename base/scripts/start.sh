15b350507fda400d356542a61841b9c272b94cd3
Max Mazur <mmazur@griddynamics.com>
2018-01-15 18:31:50 +0200

    Add teplates/configs

--- base/scripts/start.sh
+++ base/scripts/start.sh
@@ -3,5 +3,16 @@
 set -x
 env
 
+H=$(hostname -f)
+U=$(cat /proc/sys/kernel/random/uuid)
+test -f /etc/consul/consul.d/node.json ||
+
+cat <<EOF >  /etc/consul/consul.d/node.json
+{
+    "node_name": "${H}-${U}"
+}
+EOF
+
+
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
 
